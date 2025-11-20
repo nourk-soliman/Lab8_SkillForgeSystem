01228bff1ae425149f133f058e7e805604f0c24f
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-11-20 17:11:55 +0200

    Add files via upload

---
+++ src/json/JsonCourses.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Json;
+import model.course.Course;
+import model.course.Lesson;
+import com.google.gson.*;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JsonCourses implements JsonTranslator{
+
+    private List<Course> courses = new ArrayList<>();
+    private List<Lesson> lessons = new ArrayList<>();
+    private String filename;
+
+    public JsonCourses(String filename) {
+        this.filename = filename;
+        readFromFile(filename);
+    }
+
+    @Override
+    public void readFromFile(String filename) {
+        try {
+            Gson gson = new Gson();//is an instance of the Gson class from the Gson library//
+            FileReader reader = new FileReader(filename);
+
+            JsonArray jsonArray = JsonParser.parseReader(reader).getAsJsonArray();// reads JSON array from file//
+            reader.close();
+
+            for (JsonElement element : jsonArray) {
+                JsonObject obj = element.getAsJsonObject();// converts each element to a JsonObject so you can access it by feild names//
+                if (obj.has("courseId")) {
+                    Course course = gson.fromJson(element, Course.class);
+                    courses.add(course);
+
+                    if (obj.has("lessons")) {
+                        JsonArray lessonsArray = obj.getAsJsonArray("lessons");
+
+                        for (JsonElement lessonElement : lessonsArray) {
+                            Lesson lesson = gson.fromJson(lessonElement, Lesson.class);
+                            lessons.add(lesson);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Error: Error while reading the filesa");
+            e.printStackTrace();
+        }
+    }
+
+    public void setCourses(List<Course> courses) {
+        this.courses = courses;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    @Override
+    public void saveToFile(String filename) {
+        try {
+            Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+            JsonArray jsonArray = new JsonArray();
+
+            for (Course course : courses) {
+                jsonArray.add(gson.toJsonTree(course));
+            }
+
+            FileWriter writer = new FileWriter(filename);
+            gson.toJson(jsonArray, writer);
+            writer.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<Course> getCourses() {
+        return courses;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+
+}
