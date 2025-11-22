2ae4d2fd8db1edbc8bd2b52be6d0829bd0a3de2a
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-11-22 17:51:46 +0200

    Add files via upload

--- src/gui/student/QuizPage.java
+++ src/gui/student/QuizPage.java
@@ -4,26 +4,35 @@
  */
 package gui.student;
 
+import Json.CoursesDatabase;
+import java.util.ArrayList;
 import model.course.Lesson;
-import model.user.Student;
+import model.quiz.Quiz;
 
 /**
  *
  * @author Nour
  */
 public class QuizPage extends javax.swing.JFrame {
-    private Student s;
     private Lesson lesson;
+    
     private static final java.util.logging.Logger logger = java.util.logging.Logger.getLogger(QuizPage.class.getName());
 
     /**
      * Creates new form QuizPage
-     * @param s
-     * @param selectedLesson
      */
-    public QuizPage(Student s, Lesson selectedLesson) {
+    public QuizPage(Lesson lesson) {
+        this.lesson=lesson;
         initComponents();
     }
+public void uploadQuiz()
+{Quiz quiz=lesson.getQuiz();
+ArrayList<QuizQuestions> questions=quiz.getQuestions();
+
+
+
+
+}
 
     /**
      * This method is called from within the constructor to initialize the form.
@@ -34,39 +43,17 @@ public class QuizPage extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jScrollPane1 = new javax.swing.JScrollPane();
-        jTable1 = new javax.swing.JTable();
-
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jTable1.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
-            },
-            new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
-            }
-        ));
-        jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(19, Short.MAX_VALUE)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 375, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+            .addGap(0, 400, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 275, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(19, Short.MAX_VALUE))
+            .addGap(0, 300, Short.MAX_VALUE)
         );
 
         pack();
@@ -75,10 +62,30 @@ public class QuizPage extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-  
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ReflectiveOperationException | javax.swing.UnsupportedLookAndFeelException ex) {
+            logger.log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        /*java.awt.EventQueue.invokeLater(() -> new QuizPage(Lesson lesson).setVisible(true));*/
+      
+        
+    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JTable jTable1;
     // End of variables declaration//GEN-END:variables
 }
