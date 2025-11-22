0bc315379e3fcac5c8b5bbcd79c89ab2e0850e58
nourk-soliman <es.nour.soliman2023@alexu.edu.eg>
2025-11-22 17:50:55 +0200

    Add files via upload

--- src/model/quiz/Quiz.java
+++ src/model/quiz/Quiz.java
@@ -3,11 +3,43 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package model.quiz;
+import java.util.ArrayList;
 
 /**
  *
  * @author Nour
  */
 public class Quiz {
+ private ArrayList<QuizQuestion> questions;
+
+    public Quiz(ArrayList<QuizQuestion> questions) {
+        try{
+        setQuestions(questions);}
+        catch(IllegalArgumentException e)
+        {}
+        
+    }
+
+    public ArrayList<QuizQuestion> getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(ArrayList<QuizQuestion> questions) {
+        if(questions==null||questions.isEmpty())
+        {throw new IllegalArgumentException("Questions can not be empty."); }
+        this.questions = questions;
+    }
+ 
+
+    public static void main(String[] args) {
+        ArrayList<QuizQuestion> questions=new ArrayList<>();
+        ArrayList<String> options=new ArrayList<>();
+        options.add("one.");
+        options.add("two.");
+        questions.add(new QuizQuestion("How many fundamentals are there?",1,options));
+        for(QuizQuestion question:questions)
+            question.displayInfo();
+        Quiz q=new Quiz(questions);
+    }
     
 }
--- src/model/quiz/QuizQuestion.java
+++ src/model/quiz/QuizQuestion.java
@@ -4,10 +4,75 @@
  */
 package model.quiz;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Nour
  */
 public class QuizQuestion {
+    private String question;
+    private int answerIndex;
+    private ArrayList<String>options;
+
+    public QuizQuestion(String question, int answerIndex, ArrayList<String> options) {
+        try{
+        setQuestion(question);
+        setOptions(options);
+        setAnswerIndex(answerIndex);
+        }
+  catch(IllegalArgumentException e)
+  {System.out.println("Invalid formats.");}
+    }
+
+    public int getAnswerIndex() {
+        return answerIndex;
+    }
+
+    public void setAnswerIndex(int answerIndex) {
+        if(answerIndex<0)
+        {throw new IllegalArgumentException("Invalid index.");}
+        if(answerIndex>(options.size()-1))
+        {throw new IllegalArgumentException("Index is out of bounds.");}
+        this.answerIndex = answerIndex;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        if(question==null|| question.isBlank())
+        {throw new IllegalArgumentException("Question can not be empty.");}
+        if(question.length()<5)
+        {throw new IllegalArgumentException("Question should be at least 5 characters."); }
+        this.question = question;
+    }
+
+    
+
+    public ArrayList<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(ArrayList<String> options) {
+        if(options==null|| options.isEmpty())
+        {throw new IllegalArgumentException("Options should not be left blank.");}
+        if(options.size()<2)
+        {throw new IllegalArgumentException("Options should be at least 2."); }
+        for(String option:options)
+        {if(option.length()<2)
+        {throw new IllegalArgumentException("Option length should be at least 5."); }}
+        this.options = options;
+    }
+    public void displayInfo()
+    {
+        System.out.println("Question: "+question);
+        for(String option:options)
+        System.out.println("Options: "+option);
+        System.out.println("Answer: "+options.get(answerIndex));
+    
+    
+    }
     
 }
